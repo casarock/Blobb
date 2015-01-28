3270ff5322c19a4cb844a4d04d01c26e5c49e3bd
Carsten Sandtner <sandtner@mediaman.de>
2015-01-28 10:41:53 +0100

    prototype works now.

--- Game.js
+++ Game.js
@@ -1,5 +1,4 @@
 RunSanta.Game = function(game) {
-
 	this.game; //	a reference to the currently running game
 	this.add; //	used to add sprites, text, groups, etc
 	this.camera; //	a reference to the game camera
@@ -16,66 +15,76 @@ RunSanta.Game = function(game) {
 	this.particles; //	the particle manager
 	this.physics; //	the physics manager
 	this.rnd; //	the repeatable random number generator
-
 };
 
 RunSanta.Game.prototype = {
 
 	create: function() {
-
 		this.game.stage.backgroundColor = '#000000';
-		this.blobbs = [];
-
 		this.game.physics.startSystem(Phaser.Physics.ARCADE);
-		this.bubble = this.add.sprite(100, 100, 'bubble');
-		this.bubble.inputEnabled = true;
-		this.bubble.scale.set(0.12);
-		this.bubble.anchor.set(0.5);
 
-		this.bubble.events.onInputDown.add(this.particleBurst_circle, this);
+		this.blobbs = this.add.group();
+		this.blobbs.enableBody = true;
+		this.blobbs.physicsBodyType = Phaser.Physics.ARCADE;
+
+		this.bubbles = this.add.group();
+		this.bubbles.enableBody = true;
+		this.bubbles.physicsBodyType = Phaser.Physics.ARCADE;
+
+		for (var i = 0; i < 20; i++) {
+			var b = this.bubbles.create(32 + Math.random() * 420, 32 + Math.random() * 300, 'bubble');
+			b.scale.set(0.12);
+			b.anchor.set(0.5);
+		}
+
+		this.input.onDown.add(this.burstCircle, this);
 	},
 
 	update: function() {
+		this.blobbs.forEachAlive(function(particleGroup) {
+			this.game.physics.arcade.overlap(particleGroup, this.bubbles, this.collisionHandler, null, this);
+		}, this);
+	},
 
+	collisionHandler: function(snow, bubble) {
+		this.destroyBubble(bubble);
 	},
 
 	quitGame: function(pointer) {
 		this.state.start('MainMenu');
 	},
 
-	particleBurst_circle: function(bubble, pointer) {
-
-		var emitter = this.add.emitter(bubble.position.x, bubble.position.y, 20);
-		
-
-		console.log(emitter);
-
-		emitter.makeParticles('snow');
-		emitter.gravity = 0;
-
-		emitter.x = bubble.x;
-		emitter.y = bubble.y;
+	destroyBubble: function(bubble) {
+		this.burstCircle(bubble);
 		bubble.destroy();
+	},
 
+	burstCircle: function(bubble, pointer) {
+		var particles = this.add.group(),
+			startX = bubble.x,
+			startY = bubble.y,
+			EXPLODE_DIAMETER = 50.0;
 
-		var EXPLODE_DIAMETER = 50.0;
-
-		// emit a circle of particles, 360 / 18 = 20 particles
+		// emit a circle of snow flakes!, 360 / 18 = 20 particles
 		for (var i = 0; i < 360; i = i + 18) {
-			// set fixed x speed
-			var xsp = Math.cos(2 * Math.PI * i / 360.0) * EXPLODE_DIAMETER;
-			emitter.setXSpeed(xsp, xsp);
+			var xsp = Math.cos(2 * Math.PI * i / 360.0) * EXPLODE_DIAMETER,
+				ysp = Math.sin(2 * Math.PI * i / 360.0) * EXPLODE_DIAMETER;
+				particle = particles.create(startX, startY, 'snow');
 
-			// set fixed y speed
-			var ysp = Math.sin(2 * Math.PI * i / 360.0) * EXPLODE_DIAMETER;
-			emitter.setYSpeed(ysp, ysp);
+			this.physics.enable(particle, Phaser.Physics.ARCADE);
 
-			// next line doesn't work, only emits one(?) moving particle
-			emitter.start(true, 1000, null, 1);
-			emitter.update();
-			// next line add particles, but not moving
-			//var star = emitter.create(pointer.x + xsp, pointer.y + ysp, 'snow', null, true);
+			particle.body.velocity = {
+				x: xsp,
+				y: ysp
+			};
 		}
-	}
 
+		this.blobbs.add(particles);
+
+		// And remove it!
+		this.game.time.events.add(Phaser.Timer.SECOND * 1, function() {
+			particles.destroy();
+		}, this);
+
+	}
 };

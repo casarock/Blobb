0c8705bfd1957648dbf9bab0d2595ae40c000f99
Carsten Sandtner <sandtner@mediaman.de>
2015-01-30 14:59:08 +0100

    Switch to mobile portrait mode. added basic gameplay.

--- Game.js
+++ Game.js
@@ -1,24 +1,8 @@
 RunSanta.Game = function(game) {
-	this.game; //	a reference to the currently running game
-	this.add; //	used to add sprites, text, groups, etc
-	this.camera; //	a reference to the game camera
-	this.cache; //	the game cache
-	this.input; //	the global input manager (you can access this.input.keyboard, this.input.mouse, as well from it)
-	this.load; //	for preloading assets
-	this.math; //	lots of useful common math operations
-	this.sound; //	the sound manager - add a sound, play one, set-up markers, etc
-	this.stage; //	the game stage
-	this.time; //	the clock
-	this.tweens; //  the tween manager
-	this.state; //	the state manager
-	this.world; //	the game world
-	this.particles; //	the particle manager
-	this.physics; //	the physics manager
-	this.rnd; //	the repeatable random number generator
+
 };
 
 RunSanta.Game.prototype = {
-
 	create: function() {
 		this.game.stage.backgroundColor = '#000000';
 		this.game.physics.startSystem(Phaser.Physics.ARCADE);
@@ -31,13 +15,82 @@ RunSanta.Game.prototype = {
 		this.bubbles.enableBody = true;
 		this.bubbles.physicsBodyType = Phaser.Physics.ARCADE;
 
-		for (var i = 0; i < 20; i++) {
-			var b = this.bubbles.create(32 + Math.random() * 420, 32 + Math.random() * 300, 'bubble');
-			b.scale.set(0.12);
-			b.anchor.set(0.5);
-		}
+		this.createBubblesPool(64);
+		this.spawnBubbles(25, 50, 4);
+
+		this.timer = this.game.time.create(false);
+		this.timer.add(1000, this.spawnBubble, this);
+		this.timer.start();
+
+		this.passedSeconds = 0;
+
+		this.diameterMin = 25;
+		this.diameterMax = 50;
 
 		this.input.onDown.add(this.burstCircle, this);
+	},
+
+	createBubblesPool: function(num) {
+		for (var i = 0; i < num; i++) {
+			var b = this.bubbles.create(32 + Math.random() * 420, -10, 'bubble');
+			b.kill();
+		}
+	},
+
+	spawnBubble: function() {
+		var numBubbles = 1,
+			minSpeed = 25,
+			maxSpeed = 50,
+			timeToSpawn = 1000;
+
+		this.passedSeconds++;
+		if (this.passedSeconds > 5 && this.passedSeconds < 10) {
+			numBubbles = 3;
+			minSpeed = 50;
+			maxSpeed = 100;
+			timeToSpawn = 800;
+
+		} else if (this.passedSeconds > 10 && this.passedSeconds < 20) {
+			numBubbles = 5;
+			minSpeed = 50;
+			maxSpeed = 150;
+			timeToSpawn = 600;
+		} else if (this.passedSeconds > 20 && this.passedSeconds < 30) {
+			numBubbles = 5;
+			minSpeed = 100;
+			maxSpeed = 200;
+			timeToSpawn = 800;
+			this.diameterMin = 20;
+			this.diameterMax = 35;
+		} else if (this.passedSeconds > 30) {
+			numBubbles = 5;
+			minSpeed = 150;
+			maxSpeed = 250;
+			timeToSpawn = 1000;
+			this.diameterMin = 20;
+			this.diameterMax = 25;
+		}
+
+		this.spawnBubbles(minSpeed, maxSpeed, numBubbles);
+		this.timer.add(1000, this.spawnBubble, this);
+		this.timer.start();
+	},
+
+	spawnBubbles: function(minSpeed, maxSpeed, num) {
+		num = num || 1;
+
+		for (var i = 0; i < num; i++) {
+			var bubble = this.bubbles.getFirstDead();
+			if (bubble === null || bubble === undefined) return;
+
+			bubble.revive();
+			bubble.checkWorldBounds = true;
+			bubble.outOfBoundsKill = true;
+			bubble.reset(20 + Math.random() * 300, -10);
+			bubble.scale.set(0.12);
+			bubble.anchor.set(0.5);
+			bubble.body.velocity.y = this.rnd.integerInRange(minSpeed, maxSpeed);
+		}
 	},
 
 	update: function() {
@@ -56,14 +109,14 @@ RunSanta.Game.prototype = {
 
 	destroyBubble: function(bubble) {
 		this.burstCircle(bubble);
-		bubble.destroy();
+		bubble.kill();
 	},
 
 	burstCircle: function(bubble, pointer) {
 		var particles = this.add.group(),
 			startX = bubble.x,
 			startY = bubble.y,
-			EXPLODE_DIAMETER = 50.0;
+			EXPLODE_DIAMETER = this.rnd.integerInRange(this.diameterMin, this.diameterMax);
 
 		// emit a circle of snow flakes!, 360 / 18 = 20 particles
 		for (var i = 0; i < 360; i = i + 18) {
@@ -86,5 +139,17 @@ RunSanta.Game.prototype = {
 			particles.destroy();
 		}, this);
 
-	}
+	},
+	render: function() {
+		/* this.game.debug.body(this.planes[0].getSprite());
+		this.game.debug.body(this.planes[1].getSprite());
+		this.tubey.getGroup().forEachAlive(this.renderGroup, this);
+		*/
+		//this.bubbles.forEachAlive(this.renderGroup, this);
+
+	},
+
+	renderGroup: function(member) {
+		this.game.debug.body(member);
+	},
 };
